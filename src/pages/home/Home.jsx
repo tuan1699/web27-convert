a66e843294d46fd44909e2ab3bea78b5d1445796
tuandang <tuandangvan1699@gmail.com>
2022-11-18 18:52:35 +0700

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Diet from "./components/diet/Diet";
 import Banner from "./components/banner/Banner";
 import RecipesField from "./components/recipes/RecipesField";
 import SuggestField from "./components/suggest/SuggestField/SuggestField";
-import QuickField from "./components/quickMeal/quickField/quickField";
+import QuickField from "./components/quickMeal/quickField/QuickField";
 import BlogField from "./components/blogs/blogField/BlogField";
 import CourseField from "./components/course/courseField/CourseField";
 
